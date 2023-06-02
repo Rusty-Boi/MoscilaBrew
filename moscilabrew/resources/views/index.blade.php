1e58ddb4a06522a25b8beedcb083948f4ecac1b1
Benngki <benediktus.setiawan@binus.ac.id>
2023-06-02 15:00:53 +0700

    header update

--- moscilabrew/resources/views/index.blade.php
+++ moscilabrew/resources/views/index.blade.php
@@ -6,30 +6,33 @@
 @endsection
 
 @section('content')
-    <!-- Carousel -->
-    <div class="slider nav-on pt-0">
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active vh-90">
-            <img src={{asset('/img/coffee-18.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
+
+    <!-- Carousel Section - Home Page -->
+    <section id="carousel" class="carousel">
+      <div class="slider nav-on pt-0">
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active vh-90">
+              <img src={{asset('/img/coffee-18.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
+            </div>
+            <div class="carousel-item vh-90">
+              <img src={{asset('/img/Coffee-wallpaper-hd-wallpapers-download.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
+            </div>
+            <div class="carousel-item vh-90">
+              <img src={{asset('/img/R.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
+            </div>
           </div>
-          <div class="carousel-item vh-90">
-            <img src={{asset('/img/Coffee-wallpaper-hd-wallpapers-download.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
-          </div>
-          <div class="carousel-item vh-90">
-            <img src={{asset('/img/R.jpg')}} class="d-block w-100" alt="..." style="background-position: center">
-          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
+    </section>
       
     <!-- End of Carousel -->
 
@@ -46,6 +49,22 @@
         <div class="qualityStorage">
           <h3>Quality Storage</h3>
           <h5>High quality storage system</h5>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="container text-center">
+        <div class="row row-cols-2 row-cols-lg-3 g-2 g-lg-3 bg-krim">
+          <div class="col">
+            <div class="card">
+              <img class="w-20 align-self-center" src="{{asset('img/freshly-roasted.png')}}" class="card-img-top" alt="..." style="background-color:  !important">
+              <div class="card-body">
+                <h5 class="card-title">Freshly Roasted</h5>
+                <p class="card-text">Roasted 12 hours before shipment</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- moscilabrew/resources/views/layouts/header.blade.php
+++ moscilabrew/resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-  <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary vh-10">
+  <nav class="navbar fixed-top navbar-expand-lg bg-secondary vh-10">
     <div class="container-fluid">
       <a class="navbar-brand mr-20 fw-bold" href="/">MoscilaBrew</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,33 +9,33 @@
         <ul class="navbar-nav nav-underline me-auto mb-2 mb-lg-0 m-auto">
             <li class="nav-item">
               <a class="nav-link {{ (request()->is('/')) ? 'active' : '' }}" href="/">Home</a>
+              <div class="border-bottom"></div>
             </li>
             <li class="nav-item">
               <a class="nav-link {{ (request()->is('catalog')) ? 'active' : '' }}" href="/catalog">Catalog</a>
+              <div class="border-bottom"></div>
             </li>
             <li class="nav-item">
-              <a class="nav-link {{ (request()->is('featured')) ? 'active' : '' }}" href="/featured">Featured</a>
+              <a class="nav-link {{ (request()->is('featured')) ? 'active' : '' }}" href="/featured">Blend</a>
+              <div class="border-bottom"></div>
             </li>
             <li class="nav-item">
               <a class="nav-link {{ (request()->is('aboutUs')) ? 'active' : '' }}" href="/aboutUs">About Us</a>
+              <div class="border-bottom"></div>
             </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button onclick="search()" class="btn" type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-          </button>
-        </form>
         <ul class="navbar-nav me-right">
-          <li class="nav-item ms-2">
-            <a name="" id="" class="btn border-2" href="/login" role="button" style="border-color: rgb(239, 195, 164);">Login</a>
+          <li class="nav-item">
+            <form class="d-flex" role="search">
+              {{-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> --}}
+              <button onclick="search()" class="btn" type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+              </button>
+            </form>
           </li>
-          <li class="nav-item ms-2">
-            <a name="" id="" class="btn bg-krim" href="/register" role="button">Register</a>
-          </li>
-          <!-- <li class="nav-item">
+          <li class="nav-item">
             <button onclick="profileNav()" class="btn profile" type="button" id="userProfile">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" class="bi bi-person" viewBox="0 0 16 16">
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
@@ -48,7 +48,17 @@
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
           </button>
-          </li> -->
+          </li>
+          <li class="nav-item ms-2">
+            <a name="" id="" class="btn-partners btn border-2 bg-primary fw-bold rounded-pill px-4" href="/login" role="button">Partners</a>
+          </li>
+          {{-- <li class="nav-item ms-2">
+            <a name="" id="" class="btn border-2" href="/login" role="button" style="border-color: rgb(239, 195, 164);">Login</a>
+          </li>
+          <li class="nav-item ms-2">
+            <a name="" id="" class="btn bg-krim" href="/register" role="button">Register</a>
+          </li> --}}
+          <!--  -->
         </ul>
       </div>
     </div>
